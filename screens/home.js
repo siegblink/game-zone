5e4a1d4ee46db2b152d8c185c004cc6b9d36bef5
Sieg <siegblink@gmail.com>
2020-03-09 09:02:17 +0800

    Add pressHandler on buttons in home and reviewDetails component.

--- screens/home.js
+++ screens/home.js
@@ -1,11 +1,17 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, Button } from 'react-native'
 import { globalStyles } from '../styles/global'
 
-export default function Home() {
+export default function Home({ navigation }) {
+  function pressHandler() {
+    navigation.navigate('ReviewDetails')
+    // navigation.push('ReviewDetails')
+  }
+
   return (
     <View style={globalStyles.container}>
       <Text style={globalStyles.titleText}>Home Screen</Text>
+      <Button title='Go to review details' onPress={pressHandler} />
     </View>
   )
 }
--- screens/reviewDetails.js
+++ screens/reviewDetails.js
@@ -1,11 +1,16 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, Button } from 'react-native'
 import { globalStyles } from '../styles/global'
 
-export default function ReviewDetails() {
+export default function ReviewDetails({ navigation }) {
+  function pressHandler() {
+    navigation.goBack()
+  }
+
   return (
     <View style={globalStyles.container}>
-      <Text>Review Details Screen</Text>
+      <Text style={globalStyles.titleText}>Review Details Screen</Text>
+      <Button title='Back to home screen' onPress={pressHandler} />
     </View>
   )
 }
