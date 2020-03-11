97f584299e2b140bfecf777c43e9f8d8795a6b54
Sieg <siegblink@gmail.com>
2020-03-11 15:11:00 +0800

    Refactor reviewDetails component by adding images as rating.

--- screens/reviewDetails.js
+++ screens/reviewDetails.js
@@ -1,17 +1,31 @@
 import React from 'react'
-import { View, Text, Button } from 'react-native'
+import { View, Text, StyleSheet, Image } from 'react-native'
 import Card from '../shared/card'
-import { globalStyles } from '../styles/global'
+import { globalStyles, images } from '../styles/global'
 
 export default function ReviewDetails({ navigation }) {
-
+  const rating = navigation.getParam('rating')
   return (
     <View style={globalStyles.container}>
       <Card>
         <Text>{navigation.getParam('title')}</Text>
         <Text>{navigation.getParam('body')}</Text>
-        <Text>{navigation.getParam('rating')}</Text>
+        <View style={styles.rating}>
+          <Text>GameZone rating: </Text>
+          <Image source={images.ratings[rating]} />
+        </View>
       </Card>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  rating: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    paddingTop: 16,
+    marginTop: 16,
+    borderTopWidth: 1,
+    borderTopColor: '#eee',
+  },
+})
